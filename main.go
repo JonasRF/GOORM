18bce834cb89bac4299ebc6ef259fd7b2518944b
JonasRF <jonasribeiro41@yahoo.com.br>
2024-10-17 16:12:18 -0300

    Realizando consultas com where

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -19,10 +20,25 @@ func main() {
 	}
 	db.AutoMigrate(&Product{})
 
-	products := []Product{
-		{Name: "Notebook", Price: 2500.00},
-		{Name: "Tablet", Price: 1200.00},
-		{Name: "TV LG Smart", Price: 3400.00},
+	//products := []Product{
+	//	{Name: "Notebook", Price: 2500.00},
+	//	{Name: "Tablet", Price: 1200.00},
+	//	{Name: "TV LG Smart", Price: 3400.00},
+	//}
+	//db.Create(&products)
+
+	//var product Product
+	//db.First(&product, 2)
+	//fmt.Println(product)
+	//var products []Product
+	//db.Find(&products)
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	var products []Product
+	db.Where("name LIKE ?", "%k%").Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
 	}
-	db.Create(&products)
 }
